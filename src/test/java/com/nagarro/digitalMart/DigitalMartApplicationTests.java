cad993b6df5dc6d6627ef1d8af2e86ba8f1ae124
Kunal02nagarro <158562826+Kunal02nagarro@users.noreply.github.com>
2026-02-05 01:07:55 +0530

    Disable SpringBootTest and contextLoads method
    Comment out SpringBootTest and contextLoads test method.

--- src/test/java/com/nagarro/digitalMart/DigitalMartApplicationTests.java
+++ src/test/java/com/nagarro/digitalMart/DigitalMartApplicationTests.java
@@ -3,11 +3,11 @@ package com.nagarro.digitalMart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class DigitalMartApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
